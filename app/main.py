dc3a8541766add1e7e504e75302fc1179e51d4ad
root <96pedroelias96@gmail.com>
2024-05-13 05:55:40 +0000

    modelo con chatgpt

--- app/main.py
+++ app/main.py
@@ -1,33 +1,38 @@
+# main.py
+
 import openai
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
 from dotenv import load_dotenv
-from pathlib import Path
+import os
 
-# Load environment variables
-load_dotenv()
+# Specify the path to the .env file and load environment variables
+env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path=env_path)
 
-# Set up OpenAI client configuration
-openai.api_key = "c4129d4dad0247a691ad29bd38db1260"
-openai.api_base = "https://openaiuniandesdevtest.openai.azure.com/"
-engine = 'gpt-turbo-RF-NT'
-
+# Initialize the FastAPI app
 app = FastAPI()
 
+# Set up OpenAI client configuration with environment variables
+openai.api_key = os.getenv("OPENAI_API_KEY")
+engine = 'gpt-3.5-turbo'  # Adjust according to your subscription and needs
+
+# Define the Pydantic model for chat message input validation
 class ChatMessage(BaseModel):
     user_input: str
 
 # Function to generate text completions using the OpenAI API
-def get_completion(prompt, engine="gpt-turbo-RF-NT"):
+def get_completion(prompt, engine="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
-        engine=engine,
+        model=engine,
         messages=messages,
-        temperature=0.5  # this is the degree of randomness of the model's output
+        temperature=0.5  # Controls the randomness of the model's output
     )
     return response.choices[0].message["content"]
 
+# Define a POST endpoint for the chat interaction
 @app.post("/chat/")
 async def chat_with_agent(chat_message: ChatMessage):
     try:
@@ -36,9 +41,11 @@ async def chat_with_agent(chat_message: ChatMessage):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# Define a GET endpoint to verify server operation
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI OpenAI Integration!"}
 
+# If the script is executed as the main script, run the server
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8800, reload=True)
