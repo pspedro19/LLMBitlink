e12ecf7e0027db3d8aaa94a3a07851d066485dd0
root <96pedroelias96@gmail.com>
2024-05-21 15:59:32 +0000

    pushed

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e  # Exit the script if any statement returns a non-true return value
 
 # Crear directorios y establecer permisos
-mkdir -p /opt/airflow/logs/scheduler
+mkdir -p /opt/airflow/logs /opt/airflow/logs/scheduler
 chown -R airflow:root /opt/airflow/logs
+chmod -R 775 /opt/airflow/logs
 
 # Inicializar la base de datos de Airflow
 airflow db init
@@ -10,9 +12,12 @@ airflow db init
 # Aplicar cualquier migración necesaria
 airflow db upgrade
 
-# Instalar pendulum y aplicar parche a settings.py si es necesario
+# Instalar dependencias necesarias
 pip install --upgrade pendulum==2.1.2
-sed -i "s/TIMEZONE = pendulum.tz.timezone('UTC')/import pendulum\nTIMEZONE = pendulum.timezone('UTC')/g" /home/airflow/.local/lib/python3.9/site-packages/airflow/settings.py
 
-# Iniciar el servidor web de Airflow
+# Corregir el archivo de configuración si es necesario
+# Aplica un parche a settings.py para ajustar la configuración de la zona horaria
+sed -i "s/import pendulum\nTIMEZONE = 'UTC'/from pendulum import timezone\nTIMEZONE = timezone('UTC')/g" /home/airflow/.local/lib/python3.9/site-packages/airflow/settings.py
+
+# Comando para iniciar el servidor web de Airflow, asegurándose de que se ejecute como el último comando
 exec airflow webserver
