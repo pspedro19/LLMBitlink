8edc30f11334900f67f191e613676de10c677206
Pedro Perez <96pedroelias96@gmail.com>
2024-06-06 02:49:51 -0500

    Update entrypoint.sh

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -10,20 +10,20 @@ chown -R postgres:postgres /var/lib/postgresql/data
 chmod -R 0700 /var/lib/postgresql/data
 
 # Check if the data directory is valid
-if [ ! -f /var/lib/postgresql/data/PG_VERSION ]; then
-    echo "No valid data found in /var/lib/postgresql/data, initializing database."
+if [ ! -f /var/lib/postgresql/data/pgdata/PG_VERSION ]; then
+    echo "No valid data found in /var/lib/postgresql/data/pgdata, initializing database."
     # Check if the directory is empty or only contains non-essential files
-    if [ -z "$(ls -A /var/lib/postgresql/data | grep -vE 'pg_hba.conf|postgresql.conf')" ]; then
+    if [ -z "$(ls -A /var/lib/postgresql/data/pgdata | grep -vE 'pg_hba.conf|postgresql.conf')" ]; then
         # Initialize the database
-        su - postgres -c "$INITDB_BIN -D /var/lib/postgresql/data"
+        su - postgres -c "$INITDB_BIN -D /var/lib/postgresql/data/pgdata"
     else
-        echo "Error: /var/lib/postgresql/data is not empty and does not contain a valid PostgreSQL data directory."
+        echo "Error: /var/lib/postgresql/data/pgdata is not empty and does not contain a valid PostgreSQL data directory."
         exit 1
     fi
 fi
 
 # Start PostgreSQL using the postgres user with full path to the binary
-su - postgres -c "$POSTGRES_BIN -D /var/lib/postgresql/data &"
+su - postgres -c "$POSTGRES_BIN -D /var/lib/postgresql/data/pgdata &"
 
 # Wait for PostgreSQL to be ready
 until pg_isready -h localhost -U "$POSTGRES_USER"; do
@@ -51,4 +51,5 @@ su - postgres -c "psql -v ON_ERROR_STOP=1 --username \"$POSTGRES_USER\" --dbname
 EOSQL"
 echo "pgvector extension ensured."
 
-# Wait for the main 
+# Wait for the main process to start
+wait
