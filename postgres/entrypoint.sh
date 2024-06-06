8c847ba116af08b652666de4251be57c63b6a544
Pedro Perez <96pedroelias96@gmail.com>
2024-06-05 23:12:42 -0500

    Update entrypoint.sh

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 set -e
 
-
-if [ "$(ls -A /var/lib/postgresql/data)" ]; then
-     echo "Limpiando el directorio de datos..."
-     rm -rf /var/lib/postgresql/data/*
+# Check if the data directory is empty
+if [ -z "$(ls -A /var/lib/postgresql/data)" ]; then
+    echo "No data found in /var/lib/postgresql/data, initializing database."
+    # If no data, proceed to initialize the database
+    docker-entrypoint.sh postgres &
+else
+    echo "Data found in /var/lib/postgresql/data, using existing data."
+    # If data exists, start the PostgreSQL without initialization
+    postgres -D /var/lib/postgresql/data &
 fi
 
-
-# Start the PostgreSQL entrypoint script in the background
-docker-entrypoint.sh postgres &
-
-# Wait for PostgreSQL to start
+# Wait for PostgreSQL to be ready
 until pg_isready -h localhost -U "$POSTGRES_USER"; do
   echo "Waiting for PostgreSQL to start..."
   sleep 2
 done
+echo "PostgreSQL started successfully."
 
-# Function to create multiple databases if they do not exist
-function create_user_and_database() {
-    local database=$1
-    echo "Creating database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        SELECT 'CREATE DATABASE $database'
-        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\\gexec
-EOSQL
-}
-
-# Check if multiple databases need to be created
+# Create multiple databases if they do not exist
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
   echo "Multiple databases creation requested: $POSTGRES_MULTIPLE_DATABASES"
   for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-    create_user_and_database $db
+    echo "Creating database '$db' if it does not exist."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+        SELECT 'CREATE DATABASE $db'
+        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\\gexec
+EOSQL
   done
-  echo "Databases created"
+  echo "Databases checked/created."
 fi
 
-# Create pgvector extension if it does not exist
+# Ensure the pgvector extension is created
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS pgvector;
 EOSQL
-echo "pgvector extension created successfully."
+echo "pgvector extension ensured."
 
 # Wait for the main PostgreSQL process to finish
 wait
